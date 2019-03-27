f01e8a3034ee5d7b69c5fb62e8b83163b5dfe7e6
Uchenna <uokafor@c2gconsulting.com>
2019-03-27 15:52:35 +0100

    install gnup

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -28,6 +28,8 @@ apt-get install -y --no-install-recommends curl bzip2 bsdtar build-essential pyt
 
 dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
 
+apt-get install gnupg
+
 wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch"
 wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch.asc"
 
